4d49004d9ac6333bef8763e77862567023d4e677
Tharun R <rajtharun08@gmail.com>
2026-02-26 18:45:42 +0530

    Sprint 1: implement BookRepository for database CRUD operations

---
+++ app/repositories/book_repository.py
@@ -0,0 +1,24 @@
+from app.schemas.book_schema import BookCreate,BookResponse
+from sqlalchemy.orm import Session
+from app.models.book import Book
+from uuid import UUID
+
+class BookRepository:
+    def get_all(self,db:Session):
+        return db.query(Book).all()
+    
+    def get_by_id(self,db:Session,book_id:UUID):
+        return db.query(Book).filter(Book.id==book_id).first()
+    
+    def create(self, db: Session, book_in: BookCreate):
+        db_book = Book(
+            title=book_in.title,
+            author=book_in.author,
+            description=book_in.description,
+            price=book_in.price,
+            stock_quantity=book_in.stock_quantity
+        )
+        db.add(db_book)
+        db.commit()
+        db.refresh(db_book)
+        return db_book
