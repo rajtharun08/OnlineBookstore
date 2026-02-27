1847fbd440787fdc4deca8d19857b39b94d7b2cb
Tharun R <rajtharun08@gmail.com>
2026-02-27 19:58:06 +0530

    Sprint 4: Implement environment-based configuration using Pydantic Settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,12 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
-load_dotenv()
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-class Settings:
-    PROJECT_NAME: str = "Online Bookstore API"
-    
-    @property
-    def DATABASE_URL(self) -> str:
-        return os.getenv("DATABASE_URL")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "fallback_secret_for_dev_only")
-    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
     class Config:
-        env_file = ".env"
+        env_file = ".env" 
 
 settings = Settings()
