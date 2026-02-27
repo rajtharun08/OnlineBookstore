6c999809f671470aeb883982ec8d8d7b40450ab4
Tharun R <rajtharun08@gmail.com>
2026-02-27 20:06:13 +0530

    Sprint 4: Refactor dependencies to use custom exception

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,16 +1,17 @@
 from app.core.database import SessionLocal
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
+from jose import ExpiredSignatureError, jwt, JWTError
 from sqlalchemy.orm import Session
 from uuid import UUID
 
 from app.core.config import settings
-from app.exceptions.custom_exceptions import UnauthorizedRoleException
+from app.exceptions.custom_exceptions import OnlineBookstoreException, UnauthorizedRoleException
 from app.models.user import User
 from app.repositories.user_repository import UserRepository
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
 def get_db():
     db = SessionLocal()
     try:
@@ -19,11 +20,6 @@ def get_db():
         db.close()
         
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
         payload = jwt.decode(
             token, 
@@ -32,15 +28,18 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         )
         user_id: str = payload.get("sub")
         if user_id is None:
-            raise credentials_exception
+            raise OnlineBookstoreException(message="Invalid token payload", status_code=401)
             
+    except ExpiredSignatureError:
+        raise OnlineBookstoreException(message="Token has expired", status_code=401)
     except JWTError:
-        raise credentials_exception
+        raise OnlineBookstoreException(message="Could not validate credentials", status_code=401)
     user_repo = UserRepository()
-    user = db.query(User).filter(User.id == UUID(user_id)).first()
+    user = user_repo.get_by_id(db, UUID(user_id))
     
     if user is None:
-        raise credentials_exception
+        raise OnlineBookstoreException(message="User not found", status_code=401)
+        
     return user
 
 def role_required(allowed_roles: list[str]):
@@ -49,4 +48,3 @@ def role_required(allowed_roles: list[str]):
             raise UnauthorizedRoleException()
         return current_user
     return role_checker
-
