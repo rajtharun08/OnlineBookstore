61d5de66ccca015f520b3ebab7c4aab2d834763d
Tharun R <rajtharun08@gmail.com>
2026-02-27 09:22:47 +0530

    chore: include missing logic from previous integration

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -2,7 +2,6 @@ import re
 from app.exceptions.custom_exceptions import OnlineBookstoreException
 
 def validate_email_format(email: str):
-    """Checks if email follows standard format."""
     regex = r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     if not re.match(regex, email):
         raise OnlineBookstoreException(message="Invalid email format", status_code=400)
